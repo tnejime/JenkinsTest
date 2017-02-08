4a66279249dfa18f7205ef3b450055fc594d22c1
tnejime <tnejime@aiming-inc.com>
2017-02-08 16:33:18 +0900

    test

--- script/jenkins/lib/ansicolor.groovy
+++ script/jenkins/lib/ansicolor.groovy
@@ -10,8 +10,4 @@ def outputException (Exception e){
   outputMessage('\033[31m', 'ERROR: ' + e.message + '\n' + e.class)
 }
 
-def functionTest () {
-  println "Test Call Function is Succeed"
-}
-
 return this;
