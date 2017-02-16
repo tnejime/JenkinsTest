1c5468dbdffde9251d0480262b47589710844c54
tnejime <tnejime@aiming-inc.com>
2017-02-16 15:04:15 +0900

    test

--- script/jenkins/lib/logwrapper.groovy
+++ script/jenkins/lib/logwrapper.groovy
@@ -1,32 +1,35 @@
 #!/usr/bin/env groovy
 
-def consoleHighlight (String branch = '' , String revision = '' , Closure closure){
-    ansiColor('xterm'){
-        def result = "SUCCESS"
-        def color = '\033[32m'
-        def errorMessage = ''
-    try{
-        closure()
-    }catch (Exception e){
-        color = '\033[31m'
-        result = "FAILURE"
-        errorMessage = 'ERROR: ' + e.class + '\n' + e.message + '\n'
-        throw e
-    }finally{
-        println( 
-            color
-            + result + '\n'
-            + errorMessage
-            + 'JOB_NAME: ' + env.JOB_NAME + '\n'
-            + 'NODE_NAME: ' + env.NODE_NAME + '\n'
-            + 'NODE_LABELS: ' + env.NODE_LABELS + '\n'
-            + 'JOB_BASE_NAME' + env.JOB_BASE_NAME + '\n'
-            + 'BUILD_TAG' + env.BUILD_TAG + '\n'
-            + '\033[0m' 
-            )
-        }
-        println ('\033[34m' + "git Test")
-    }
-}
+def consoleHighlight (String status, String branch , String revision  , Exception error = null){
+  ansiColor("xterm"){
+  def color
+  def errorMessage = " "
+  
+  switch (status){
+    case "SUCCESS":
+      color = "\033[32m"
+      break
+    case "FAILURE":
+      color = "\033[31m"
+      errorMessage = error == null ? " " : "ERROR:\t" + error.class + "\nMessage:\t" + error.message + "\n"
+      break
+    default:
+      return
+  }
+  
+  println( 
+    color
+    + status + "\n"
+    + errorMessage 
+    + "JOB_NAME:\t" + env.JOB_NAME + "\n"
+    + "NODE_NAME:\t" + env.NODE_NAME + "\n"
+    + "NODE_LABELS:\t" + env.NODE_LABELS + "\n"
+    + "JOB_BASE_NAME:\t" + env.JOB_BASE_NAME + "\n"
+    + "BUILD_TAG:\t" + env.BUILD_TAG + "\n"
+    + "Git branch:\t" + branch + "\n" 
+    + "Git revision:\t" + revision + "\n"
+    + "\033[0m" 
+    )
+  }
 
 return this;
