15566e7b24d8e9327ebf56edc40abfcde09030ff
Toshiki Nejime <tnejime@aiming-inc.com>
2017-05-22 12:45:02 +0900

    test

--- work/BuildOnlyChanges.groovy
+++ work/BuildOnlyChanges.groovy
@@ -2,9 +2,6 @@
 
 config_filename = "work/build_config.json"
 
-splitmap_filename = "work/SplitHashMap.groovy"
-splitmap_script = null
-
 //変更範囲だけJenkinsにビルドさせるScripts
 node {
   stage('git checkout') {
@@ -13,32 +10,32 @@ node {
     sh "git fetch"
     sh "git checkout testdiff"
     sh "git pull origin testdiff"
-
-    splitmap_script = load(splitmap_filename)
   }
 
   stage('build on push to pull request') {
     //Gitの変更点抽出してArrayListに格納
-    def files_changed = sh(script: "git diff --stat --name-only `git show-branch --merge-base master HEAD` HEAD", returnStdout: true).split(/\n/) as ArrayList
-    def buildTargets = searchBuildTargets(files_changed)
+    def buildTargets = searchBuildTargets()
     println buildTargets
 
     def parallel_builds = []
   }
 }
 
-def searchBuildTargets(def files_changed = []) {
-  //パスを階層ごとに分割してmap keyとしてAttributeを格納する
+
+def searchBuildTargets() {
+  //configファイルからpathごとのビルド設定を格納したmapを作成する
+  def build_attribute_map = [:]
   def config_text = readFile(config_filename)
   def build_config = new groovy.json.JsonSlurperClassic().parseText(config_text)
-  def build_attribute_map = splitmap_script.createSplitHashMap("/")
-  // def build_attribute_map = new SplitHashMap()
   for (jobconfig in build_config) {
     def jobname = jobconfig["job"]
     for (attribute in ["include","exclude"]) {
       for (path in jobconfig[attribute]) {
-        //Todo build_attribute_map.putの名前直す
-        def path_attributes = build_attribute_map.put(path)
+        def path_attributes = build_attribute_map[path]
+        if(path_attributes == null) {
+          build_attribute_map[path] = [:]
+          path_attributes = build_attribute_map[path]
+        }
         if(path_attributes.containsKey(attribute)){
           path_attributes[attribute] += jobname
         } else {
@@ -49,9 +46,10 @@ def searchBuildTargets(def files_changed = []) {
   }
 
   //Git変更点からビルド対象となるJobを探す
+  def files_changed = sh(script: "git diff --stat --name-only `git show-branch --merge-base master HEAD` HEAD", returnStdout: true).split(/\n/) as ArrayList
   def buildTargets = []
   for (changed_filepath in files_changed) {
-    def path_attributes = build_attribute_map.getMatchedParameter(changed_filepath)
+    def path_attributes = getAllValueSearchThePathRecursively(build_attribute_map, changed_filepath)
     def buildtarget_cache = []
     for (attribute in path_attributes){
       if(attribute.containsKey("include")) {
@@ -69,4 +67,20 @@ def searchBuildTargets(def files_changed = []) {
   buildTargets.unique()
 
   return buildTargets
+}
+
+def getAllValueSearchThePathRecursively(def map, String fullpath, String delimiter = "/") {
+  def matched_param = []
+  def split_paths = fullpath.split(delimiter)
+  def current_path = ""
+  for (int i = 0; i < split_paths.size()-1; ++i) {
+    current_path += "${split_paths[i]}/"
+    if(map.containsKey(current_path)){
+      matched_param.add(map[current_path])
+    }
+  }
+  if(map.containsKey(fullpath)){
+    matched_param.add(map[fullpath])
+  }
+  return matched_param
 }
